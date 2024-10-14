2a33568ad9dbd51458cc4f2af9b8c1f0d51bc922
Adrian Häusler Redhe <adrian.hredhe@gmail.com>
2024-10-14 23:06:15 +0200

    feat: add nvim setup with better structuring and primagen example.

---
+++ nvim/lua/adrianhredhe-fora/options/init.lua
@@ -0,0 +1,30 @@
+vim.opt.number = true
+vim.opt.relativenumber = true
+
+vim.opt.textwidth = 60
+vim.opt.tabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.softtabstop = 4
+vim.opt.expandtab = true
+vim.opt.smartindent = true
+
+vim.opt.syntax = on
+vim.opt.ignorecase = true
+
+vim.opt.incsearch = true
+--vim.opt.nospell = true
+vim.opt.spell.spelllang = en_us
+--vim.opt.termencoding = utf8
+
+-- system clipboard
+vim.opt.clipboard = 'unnamedplus'
+
+-- highlight yank
+vim.cmd[[
+    augroup highlight_yank    
+    autocmd!
+    au TextYankPost * silent! lua vim.highlight.on_yank({timeout=150})
+    augroup end
+]]
+
+
