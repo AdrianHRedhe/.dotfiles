c40522059827c3286d5274cdcd5014b3a9b85dac
Adrian Häusler Redhe <adrian.hredhe@gmail.com>
2024-10-18 21:29:36 +0200

    feat: add keybind to search keybinds

--- nvim/lua/adrianhredhe/keybinds/keybinds.lua
+++ nvim/lua/adrianhredhe/keybinds/keybinds.lua
@@ -1,10 +1,12 @@
-
 local keymap = vim.api.nvim_set_keymap
-local opts = { noremap = true, silent = true}
+local opts = { noremap = true, silent = true }
 
 -- TELESCOPE
 -- local builtin = require('telescope')
-vim.keymap.set('n', '<leader>ff', ':Telescope find_files<CR>', { desc = 'Telescope find files' })
-vim.keymap.set('n', '<leader>fg', ':Telescope live_grep<CR>', { desc = 'Telescope find files' })
-vim.keymap.set('n', '<leader>fb', ':Telescope buffers<CR>', { desc = 'Telescope find files' })
-vim.keymap.set('n', '<leader>fh', ':Telescope help_tags<CR>', { desc = 'Telescope find files' })
+keymap("n", "<leader>ff", ":Telescope find_files<CR>", { desc = "Telescope find files" })
+keymap("n", "<leader>fg", ":Telescope live_grep<CR>", { desc = "Telescope live_grep" })
+keymap("n", "<leader>fb", ":Telescope buffers<CR>", { desc = "Telescope find buffers" })
+keymap("n", "<leader>fh", ":Telescope help_tags<CR>", { desc = "Telescope help_tags" })
+keymap("n", "<leader>fk", ":Telescope keymaps<CR>", { desc = "Telescope keymaps" })
+
+-- FROM KICKSTART
