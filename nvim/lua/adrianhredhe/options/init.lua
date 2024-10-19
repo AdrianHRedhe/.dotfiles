391d2467fd77beb4f4c625a38746da37692ae2f9
Adrian Häusler Redhe <adrian.hredhe@gmail.com>
2024-10-19 17:04:10 +0200

    feat: add cool-nvim a way to unhighlight searches

--- nvim/lua/adrianhredhe/options/init.lua
+++ nvim/lua/adrianhredhe/options/init.lua
@@ -8,23 +8,27 @@ vim.opt.softtabstop = 4
 vim.opt.expandtab = true
 vim.opt.smartindent = true
 
-vim.opt.syntax = on
+--vim.opt.syntax = on
 vim.opt.ignorecase = true
 
 vim.opt.incsearch = true
 --vim.opt.nospell = true
-vim.opt.spell.spelllang = en_us
+--vim.opt.spell.spelllang = en_us
 --vim.opt.termencoding = utf8
 
+-- undodir
+vim.opt.undodir = os.getenv("HOME") .. "/.vim/undodir"
+vim.opt.undofile = true
+
 -- system clipboard
-vim.opt.clipboard = 'unnamedplus'
+vim.opt.clipboard = "unnamedplus"
 
 -- highlight yank
-vim.cmd[[
+vim.cmd([[
     augroup highlight_yank    
     autocmd!
     au TextYankPost * silent! lua vim.highlight.on_yank({timeout=150})
     augroup end
-]]
+]])
 
-
+require("adrianhredhe.options.cool-nvim")
