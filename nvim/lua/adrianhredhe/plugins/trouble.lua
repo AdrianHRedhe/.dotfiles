1cd63229c18c194db9a37378c71c301c31f10dfb
Adrian Häusler Redhe <adrian.hredhe@gmail.com>
2024-10-20 23:28:37 +0200

    feat: change minor in trouble and which key

--- nvim/lua/adrianhredhe/plugins/trouble.lua
+++ nvim/lua/adrianhredhe/plugins/trouble.lua
@@ -2,10 +2,41 @@ return {
 	"folke/trouble.nvim",
 	config = function()
 		require("trouble").setup({
-			icons = false,
 			-- your configuration comes here
 			-- or leave it empty to use the default settings
 			-- refer to the configuration section below
 		})
 	end,
+	keys = {
+		{
+			"<leader>tx",
+			"<cmd>Trouble diagnostics toggle<cr>",
+			desc = "[T]oggle X(Trouble)",
+		},
+		-- {
+		-- 	"<leader>xX",
+		-- 	"<cmd>Trouble diagnostics toggle filter.buf=0<cr>",
+		-- 	desc = "Buffer Diagnostics (Trouble)",
+		-- },
+		-- {
+		-- 	"<leader>cs",
+		-- 	"<cmd>Trouble symbols toggle focus=false<cr>",
+		-- 	desc = "Symbols (Trouble)",
+		-- },
+		-- {
+		-- 	"<leader>cl",
+		-- 	"<cmd>Trouble lsp toggle focus=false win.position=right<cr>",
+		-- 	desc = "LSP Definitions / references / ... (Trouble)",
+		-- },
+		-- {
+		-- 	"<leader>xL",
+		-- 	"<cmd>Trouble loclist toggle<cr>",
+		-- 	desc = "Location List (Trouble)",
+		-- },
+		-- {
+		-- 	"<leader>xQ",
+		-- 	"<cmd>Trouble qflist toggle<cr>",
+		-- 	desc = "Quickfix List (Trouble)",
+		-- },
+	},
 }
