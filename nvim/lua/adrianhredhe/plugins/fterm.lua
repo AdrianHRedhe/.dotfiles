ee2f18d7dfb41093a0aeea766dc2b75fc09c7bf7
Adrian Häusler Redhe <adrian.hausler.redhe@fora.se>
2024-11-07 22:30:48 +0100

    chore: minor change to the fterm UI

--- nvim/lua/adrianhredhe/plugins/fterm.lua
+++ nvim/lua/adrianhredhe/plugins/fterm.lua
@@ -2,11 +2,11 @@ return {
 	"numToStr/FTerm.nvim",
 	config = function()
 		require("FTerm").setup({
-			border = "double",
-			dimensions = {
-				height = 0.9,
-				width = 0.9,
-			},
+			-- border = "double",
+			-- dimensions = {
+			-- 	height = 0.9,
+			-- 	width = 0.9,
+			-- },
 		})
 
 		-- Example keybindings
