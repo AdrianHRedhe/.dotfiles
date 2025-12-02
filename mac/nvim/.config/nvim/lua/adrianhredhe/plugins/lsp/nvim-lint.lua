e75f472f83a465cb80d40cf38041fae18364b5a2
Adrian Häusler Redhe <adrian.hredhe@gmail.com>
2025-12-03 00:50:15 +0100

    chore: cleanup nvim plugins

---
+++ mac/nvim/.config/nvim/lua/adrianhredhe/plugins/lsp/nvim-lint.lua
@@ -0,0 +1,32 @@
+-- linting - nvim native, complementary to LSP
+return {
+	"mfussenegger/nvim-lint",
+	event = {
+		"BufReadPre",
+		"BufNewFile",
+	},
+	config = function()
+		local lint = require("lint")
+		lint.linters_by_ft = {
+			python = { "ruff" },
+			-- markdown = { "markdownlint" },
+			-- quarto = { "markdownlint" },
+		}
+
+		-- lint.linters.markdownlint.args = {
+		-- 	"--fix",
+		-- 	"--disable",
+		-- 	"MD013",
+		-- 	"--",
+		-- }
+		--
+		local lint_augroup = vim.api.nvim_create_augroup("lint", { clear = true })
+
+		vim.api.nvim_create_autocmd({ "BufEnter", "BufWritePost", "TextChanged" }, {
+			group = lint_augroup,
+			callback = function()
+				lint.try_lint()
+			end,
+		})
+	end,
+}
