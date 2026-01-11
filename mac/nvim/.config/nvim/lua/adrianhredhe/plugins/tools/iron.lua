e232b0bcd6a8472c869f6523b806aecc68960215
Adrian Häusler Redhe <adrian.hredhe@gmail.com>
2026-01-11 23:56:06 +0100

    feat(nvim): ensure ipython runs in uv context with packages etc

--- mac/nvim/.config/nvim/lua/adrianhredhe/plugins/tools/iron.lua
+++ mac/nvim/.config/nvim/lua/adrianhredhe/plugins/tools/iron.lua
@@ -20,7 +20,14 @@ return {
 						env = { PYTHON_BASIC_REPL = "1" }, --this is needed for python3.13 and up.
 					},
 					databricks = {
-						command = { "uv", "run", "ipython", "--no-autoindent" },
+						command = {
+							"uv",
+							"run",
+							"--with",
+							"ipython",
+							"ipython",
+							"--no-autoindent",
+						},
 						block_dividers = { "# %%", "#%%", "# Databricks notebook source", "# COMMAND ----------" },
 						format = common.bracketed_paste_python,
 						env = { PYTHON_BASIC_REPL = "1" }, --this is needed for python3.13 and up.
