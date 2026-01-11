77ce73ef724825e41540b8e8f71048e066e993d4
Adrian Häusler Redhe <adrian.hredhe@gmail.com>
2026-01-11 22:50:19 +0100

    feat(nvim): iron to have command for databricks .py and cleanup of options

--- mac/nvim/.config/nvim/lua/adrianhredhe/plugins/tools/iron.lua
+++ mac/nvim/.config/nvim/lua/adrianhredhe/plugins/tools/iron.lua
@@ -8,16 +8,21 @@ return {
 		iron.setup({
 			config = {
 				scratch_repl = true,
+				close_window_on_exit = true,
 				repl_definition = {
 					sh = {
-						-- Can be a table or a function that
-						-- returns a table (see below)
 						command = { "zsh" },
 					},
 					python = {
-						command = { "python3" }, -- or { "ipython", "--no-autoindent" }
+						command = { "python3" },
+						block_dividers = { "# %%", "#%%", "# Databricks notebook source", "# COMMAND ----------" },
 						format = common.bracketed_paste_python,
-						block_dividers = { "# %%", "#%%" },
+						env = { PYTHON_BASIC_REPL = "1" }, --this is needed for python3.13 and up.
+					},
+					databricks = {
+						command = { "ipython", "--no-autoindent" },
+						block_dividers = { "# %%", "#%%", "# Databricks notebook source", "# COMMAND ----------" },
+						format = common.bracketed_paste_python,
 						env = { PYTHON_BASIC_REPL = "1" }, --this is needed for python3.13 and up.
 					},
 				},
@@ -30,35 +35,33 @@ return {
 			},
 			keymaps = {
 				toggle_repl = "<leader>ti", -- toggles the repl open and closed.
-				-- If repl_open_command is a table as above, then the following keymaps are
-				-- available
-				-- toggle_repl_with_cmd_1 = "<leader>rv",
-				-- toggle_repl_with_cmd_2 = "<leader>rh",
 				restart_repl = "<leader>iR", -- calls `IronRestart` to restart the repl
-				send_motion = "<leader>ic",
-				send_file = "<leader>if",
-				send_line = "<leader>il",
-				-- send_paragraph = "<leader>sp",
-				-- send_until_cursor = "<leader>su",
-				send_code_block = "<leader>ib",
-				send_code_block_and_move = "<leader>ir",
-				mark_motion = "<leader>mc",
-				remove_mark = "<leader>md",
+				send_motion = "<leader>im", -- [I]ron [M]otion
+				send_file = "<leader>if", -- [I]ron [F]ile
+				send_line = "<leader>il", -- [I]ron [L]ine
+				send_until_cursor = "<leader>iu", -- [I]ron [U]ntil cursor
+				send_code_block = "<leader>ib", -- [I]ron code [B]lock
+				send_code_block_and_move = "<leader>ir", -- [I]ron [R]un
+				interrupt = "<leader>ic", -- [I]ron interupt (Ctrl + [C])
+				exit = "<leader>iq", -- [I]ron [Q]uit
 				-- cr = "<leader>s<cr>",
-				interrupt = "<leader>iC",
-				exit = "<leader>iq",
 				-- clear = "<leader>cl",
 			},
-			-- If the highlight is on, you can change how it looks
-			-- For the available options, check nvim_set_hl
-			-- highlight = {
-			-- italic = true,
-			-- },
 			ignore_blank_lines = true, -- ignore blank lines when sending visual select lines
 		})
 
-		-- iron also has a list of commands, see :h iron-commands for all available commands
-		-- vim.keymap.set("n", "<leader>rf", "<cmd>IronFocus<cr>")
-		-- vim.keymap.set("n", "<leader>rh", "<cmd>IronHide<cr>")
+		vim.keymap.set("n", "<leader>id", function()
+			local bufnr = vim.api.nvim_get_current_buf()
+			local first_line = vim.api.nvim_buf_get_lines(bufnr, 0, 1, false)[1] or ""
+
+			if first_line:find("# Databricks notebook source") then
+				iron.attach("databricks", bufnr)
+				iron.send_code_block(false)
+			end
+		end, {
+			desc = "[I]ron [D]atabricks",
+			noremap = true,
+			silent = true,
+		})
 	end,
 }
