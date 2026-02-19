96b564e5211e12ab768818c212a821bdc94d495c
Adrian Häusler Redhe <adrian.hredhe@gmail.com>
2026-02-19 02:27:31 +0100

    chore(nvim): update config for cmp and auto-session due to some breaking changes in used packages

--- mac/nvim/.config/nvim/lua/adrianhredhe/plugins/lsp/nvim-cmp.lua
+++ mac/nvim/.config/nvim/lua/adrianhredhe/plugins/lsp/nvim-cmp.lua
@@ -3,31 +3,16 @@ return {
 	"hrsh7th/nvim-cmp",
 	event = "InsertEnter",
 	dependencies = {
-		{ -- Snippet Engine & its associated nvim-cmp source
-			"L3MON4D3/LuaSnip",
-			build = (function()
-				-- Build Step is needed for regex support in snippets.
-				-- This step is not supported in many windows environments.
-				-- Remove the below condition to re-enable on windows.
-				if vim.fn.has("win32") == 1 or vim.fn.executable("make") == 0 then
-					return
-				end
-				return "make install_jsregexp"
-			end)(),
-			dependencies = {
-				-- `friendly-snippets` contains a variety of premade snippets.
-				--    See the README about individual language/framework/plugin snippets:
-				--    https://github.com/rafamadriz/friendly-snippets
-				{
-					"rafamadriz/friendly-snippets",
-					config = function()
-						require("luasnip.loaders.from_vscode").lazy_load()
-					end,
-				},
-			},
+		{
+			"echasnovski/mini.snippets", -- Snippet runtime: expands triggers into code templates
+			dependencies = "rafamadriz/friendly-snippets", -- Premade snippets for most languages
+			event = "InsertEnter",
+			opts = function()
+				local snippets = require("mini.snippets")
+				return { snippets = { snippets.gen_loader.from_lang() } } -- Load snippets based on filetype
+			end,
 		},
-		"saadparwaiz1/cmp_luasnip", -- Snippet completions
-
+		"abeldekat/cmp-mini-snippets", -- nvim-cmp source for mini.snippets
 		-- Adds other completion capabilities.
 		--  nvim-cmp does not ship with all sources by default. They are split
 		--  into multiple repos for maintenance purposes.
@@ -38,13 +23,14 @@ return {
 	config = function()
 		-- See `:help cmp`
 		local cmp = require("cmp")
-		local luasnip = require("luasnip")
-		luasnip.config.setup({})
 
 		cmp.setup({
 			snippet = {
 				expand = function(args)
-					luasnip.lsp_expand(args.body)
+					local insert = MiniSnippets.config.expand.insert or MiniSnippets.default_insert
+					insert({ body = args.body })
+					cmp.resubscribe({ "TextChangedI", "TextChangedP" })
+					require("cmp.config").set_onetime({ sources = {} })
 				end,
 			},
 			completion = { completeopt = "menu,menuone,noinsert" },
@@ -80,38 +66,12 @@ return {
 				--  Generally you don't need this, because nvim-cmp will display
 				--  completions whenever it has completion options available.
 				["<C-Space>"] = cmp.mapping.complete({}),
-
-				-- Think of <c-l> as moving to the right of your snippet expansion.
-				--  So if you have a snippet that's like:
-				--  function $name($args)
-				--    $body
-				--  end
-				--
-				-- <c-l> will move you to the right of each of the expansion locations.
-				-- <c-h> is similar, except moving you backwards.
-				["<C-l>"] = cmp.mapping(function()
-					if luasnip.expand_or_locally_jumpable() then
-						luasnip.expand_or_jump()
-					end
-				end, { "i", "s" }),
-				["<C-h>"] = cmp.mapping(function()
-					if luasnip.locally_jumpable(-1) then
-						luasnip.jump(-1)
-					end
-				end, { "i", "s" }),
-
-				-- For more advanced Luasnip keymaps (e.g. selecting choice nodes, expansion) see:
-				--    https://github.com/L3MON4D3/LuaSnip?tab=readme-ov-file#keymaps
 			}),
 			sources = {
-				{
-					name = "lazydev",
-					-- set group index to 0 to skip loading LuaLS completions as lazydev recommends it
-					group_index = 0,
-				},
+				{ name = "lazydev", group_index = 0 },
 				{ name = "nvim_lsp" }, -- LSP completions
 				{ name = "buffer" }, -- Buffer completions
-				{ name = "luasnip" }, -- Snippet completions
+				{ name = "mini_snippets" }, -- Snippet completions
 				{ name = "path" }, -- Path completions
 			},
 
