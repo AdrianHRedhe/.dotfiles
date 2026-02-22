3b848808a4b496452129752ca680e38e3deead3b
Adrian Häusler Redhe <adrian.hredhe@gmail.com>
2026-02-22 16:01:59 +0100

    fix: make it so that database_paths does not complain if no postgres binary is found

--- mac/shell/.shell/paths/database_paths.sh
+++ mac/shell/.shell/paths/database_paths.sh
@@ -1,7 +1,12 @@
 # Picks Postgres up and uses the first version found on computer
-for d in /opt/homebrew/opt/postgresql@*/bin; do
-    [ -d "$d" ] && _pg_found="$d"
-done
+if [ -d /opt/homebrew/opt ]; then
+    for d in /opt/homebrew/opt/postgresql@*/bin; do
+        if [ -d "$d" ]; then
+            _pg_found="$d"
+            break
+        fi
+    done
+fi
 if [ -n "$_pg_found" ]; then
     export PATH="$_pg_found:$PATH"
 fi
