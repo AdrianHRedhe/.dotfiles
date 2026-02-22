e75e7f25a090d73687555edcd8045c367ef2ff93
Adrian Häusler Redhe <adrian.hredhe@gmail.com>
2026-02-22 16:26:09 +0100

    feat(nvim): nvim-metals plugin for scala development + scala general improvements

---
+++ mac/nvim/.config/nvim/lua/adrianhredhe/plugins/lsp/nvim-metals.lua
@@ -0,0 +1,19 @@
+return {
+	"scalameta/nvim-metals",
+	ft = { "scala", "java" },
+	opts = function()
+		local metals_config = require("metals").bare_config()
+		metals_config.capabilities = require("cmp_nvim_lsp").default_capabilities()
+		return metals_config
+	end,
+	config = function(self, metals_config)
+		local nvim_metals_group = vim.api.nvim_create_augroup("nvim-metals", { clear = true })
+		vim.api.nvim_create_autocmd("FileType", {
+			pattern = self.ft,
+			callback = function()
+				require("metals").initialize_or_attach(metals_config)
+			end,
+			group = nvim_metals_group,
+		})
+	end,
+}
