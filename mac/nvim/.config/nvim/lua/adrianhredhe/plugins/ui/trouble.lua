a977798bc71d2e8dd19d848e2ea757d3917b3c41
Adrian Häusler Redhe <63550760+AdrianHRedhe@users.noreply.github.com>
2026-02-28 15:11:25 +0100

    fix(nvim): make telescope and trouble play along nicely (#44)

--- mac/nvim/.config/nvim/lua/adrianhredhe/plugins/ui/trouble.lua
+++ mac/nvim/.config/nvim/lua/adrianhredhe/plugins/ui/trouble.lua
@@ -11,18 +11,6 @@ return {
 				scratch = false,
 			},
 		})
-
-		-- Map so that telescope can send to trouble
-		local open_with_trouble = require("trouble.sources.telescope").open
-		local telescope = require("telescope")
-		telescope.setup({
-			defaults = {
-				mappings = {
-					i = { ["<c-t>"] = open_with_trouble },
-					n = { ["<c-t>"] = open_with_trouble },
-				},
-			},
-		})
 	end,
 	keys = {
 		{
