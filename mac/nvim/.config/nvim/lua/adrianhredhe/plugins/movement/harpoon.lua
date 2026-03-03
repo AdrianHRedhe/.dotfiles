c4e706bfde2f8c18be0538f0cdb7bfe4c1d575e3
Adrian Häusler Redhe <63550760+AdrianHRedhe@users.noreply.github.com>
2026-03-04 00:05:53 +0100

    feat(nvim): update harpoon plugin with more sensible maps + settings (#46)

--- mac/nvim/.config/nvim/lua/adrianhredhe/plugins/movement/harpoon.lua
+++ mac/nvim/.config/nvim/lua/adrianhredhe/plugins/movement/harpoon.lua
@@ -2,6 +2,10 @@ return {
 	"ThePrimeagen/harpoon",
 	branch = "harpoon2",
 	dependencies = { "nvim-lua/plenary.nvim" },
+	settings = {
+		save_on_toggle = true,
+		sync_on_ui_close = true,
+	},
 
 	config = function()
 		local harpoon = require("harpoon")
@@ -12,24 +16,24 @@ return {
 
 		vim.keymap.set("n", "<leader>ha", function()
 			harpoon:list():add()
-		end)
+		end, { desc = "[H]arpoon [A]dd" })
 
-		--vim.keymap.set("n", "<leader>he", function()
-		--		harpoon.ui:toggle_quick_menu(harpoon:list())
-		--end)
+		vim.keymap.set("n", "<leader>he", function()
+			harpoon.ui:toggle_quick_menu(harpoon:list())
+		end, { desc = "[H]arpoon [E]dit marks" })
 
 		vim.keymap.set("n", "<leader>h1", function()
 			harpoon:list():select(1)
-		end)
-		vim.keymap.set("n", "<leader>h2>", function()
+		end, { desc = "[H]arpoon quickselect [1]" })
+		vim.keymap.set("n", "<leader>h2", function()
 			harpoon:list():select(2)
-		end)
-		vim.keymap.set("n", "<leader>h3>", function()
+		end, { desc = "[H]arpoon quickselect [2]" })
+		vim.keymap.set("n", "<leader>h3", function()
 			harpoon:list():select(3)
-		end)
-		vim.keymap.set("n", "<leader>h4>", function()
+		end, { desc = "[H]arpoon quickselect [3]" })
+		vim.keymap.set("n", "<leader>h4", function()
 			harpoon:list():select(4)
-		end)
+		end, { desc = "[H]arpoon quickselect [4]" })
 
 		-- basic telescope configuration
 		local conf = require("telescope.config").values
@@ -53,7 +57,7 @@ return {
 
 		vim.keymap.set("n", "<leader>ht", function()
 			toggle_telescope(harpoon:list())
-		end, { desc = "Open harpoon window" })
+		end, { desc = "Open [H]arpoon [T]ab window" })
 
 		-- Toggle previous & next buffers stored within Harpoon list
 		vim.keymap.set("n", "<C-S-P>", function()
