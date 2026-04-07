f60a56ad1f469e68c8152193f335abe5b7d5bc5d
Adrian Häusler Redhe <adrian.hredhe@gmail.com>
2026-04-07 23:03:07 +0200

    feat(nvim,tmux): switching back to default hjkl instead of jklö for navigation + copy improvement

--- mac/nvim/.config/nvim/lua/adrianhredhe/keybinds/keybinds.lua
+++ mac/nvim/.config/nvim/lua/adrianhredhe/keybinds/keybinds.lua
@@ -7,9 +7,14 @@ keymap("n", "<C-L>", ":bnext<CR>", opts)
 
 -- Hover if you want to redefine K
 keymap("n", "<space>k", "K", opts)
-keymap("n", "√", "<c-w>h", opts) -- This is Alt-j
-keymap("n", "ª", "<c-w>k", opts) -- This is Alt-k
-keymap("n", "ﬁ", "<c-w>j", opts) -- This is Alt-l
-keymap("n", "ø", "<c-w>l", opts) -- This is Alt-ö
+-- keymap("n", "√", "<c-w>h", opts) -- This is Alt-j
+-- keymap("n", "ª", "<c-w>k", opts) -- This is Alt-k
+-- keymap("n", "ﬁ", "<c-w>j", opts) -- This is Alt-l
+-- keymap("n", "ø", "<c-w>l", opts) -- This is Alt-ö
 keymap("n", "<C-j>", "J", opts)
 keymap("n", "Ö", "i<CR><esc>k$", opts)
+
+keymap("n", "˛", "<c-w>h", opts) -- This is Alt-h
+keymap("n", "√", "<c-w>k", opts) -- This is Alt-j
+keymap("n", "ª", "<c-w>j", opts) -- This is Alt-k
+keymap("n", "ﬁ", "<c-w>l", opts) -- This is Alt-l
--- mac/nvim/.config/nvim/lua/adrianhredhe/plugins/movement/tmux_navigator.lua
+++ mac/nvim/.config/nvim/lua/adrianhredhe/plugins/movement/tmux_navigator.lua
@@ -9,9 +9,13 @@ return {
 		"TmuxNavigatorProcessList",
 	},
 	keys = {
-		{ "√", "<cmd>TmuxNavigateLeft<cr>", desc = "Navigate Left" },
-		{ "ª", "<cmd>TmuxNavigateDown<cr>", desc = "Navigate Down" },
-		{ "ﬁ", "<cmd>TmuxNavigateUp<cr>", desc = "Navigate Up" },
-		{ "ø", "<cmd>TmuxNavigateRight<cr>", desc = "Navigate Right" },
+		-- { "√", "<cmd>TmuxNavigateLeft<cr>", desc = "Navigate Left" },
+		-- { "ª", "<cmd>TmuxNavigateDown<cr>", desc = "Navigate Down" },
+		-- { "ﬁ", "<cmd>TmuxNavigateUp<cr>", desc = "Navigate Up" },
+		-- { "ø", "<cmd>TmuxNavigateRight<cr>", desc = "Navigate Right" },
+		{ "˛", "<cmd>TmuxNavigateLeft<cr>", desc = "Navigate Left" },
+		{ "√", "<cmd>TmuxNavigateDown<cr>", desc = "Navigate Down" },
+		{ "ª", "<cmd>TmuxNavigateUp<cr>", desc = "Navigate Up" },
+		{ "ﬁ", "<cmd>TmuxNavigateRight<cr>", desc = "Navigate Right" },
 	},
 }
