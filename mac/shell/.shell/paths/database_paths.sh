71addd05c11b89d17e4447f2127eb554688a9681
Adrian Häusler Redhe <adrian.hredhe@gmail.com>
2026-04-18 17:32:33 +0200

    feat: add fallback path for unixodbc connection on mac silicon

--- mac/shell/.shell/paths/database_paths.sh
+++ mac/shell/.shell/paths/database_paths.sh
@@ -3,3 +3,6 @@ for _pg_dir in /opt/homebrew/opt/postgresql@*/bin; do
     [ -d "$_pg_dir" ] && export PATH="$_pg_dir:$PATH" && break
 done
 unset _pg_dir
+
+# Also add this if you have installed unixODBC on a mac silicon
+export DYLD_FALLBACK_LIBRARY_PATH=/opt/homebrew/lib
