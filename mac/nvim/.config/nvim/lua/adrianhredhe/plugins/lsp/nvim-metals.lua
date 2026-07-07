70a56e15f9d49654a2f8a0707d9178f8c31c4084
Adrian Häusler Redhe <63550760+AdrianHRedhe@users.noreply.github.com>
2026-07-07 02:13:20 +0200

    feat(nvim): scala related updates to autocomplete and debugger (dap) (#60)

--- mac/nvim/.config/nvim/lua/adrianhredhe/plugins/lsp/nvim-metals.lua
+++ mac/nvim/.config/nvim/lua/adrianhredhe/plugins/lsp/nvim-metals.lua
@@ -1,9 +1,48 @@
 return {
 	"scalameta/nvim-metals",
 	ft = { "scala", "java" },
+	dependencies = {
+		"mfussenegger/nvim-dap",
+	},
 	opts = function()
 		local metals_config = require("metals").bare_config()
 		metals_config.capabilities = require("cmp_nvim_lsp").default_capabilities()
+
+		-- Debug settings if you're using nvim-dap
+		local dap = require("dap")
+
+		dap.configurations.scala = {
+			{
+				type = "scala",
+				request = "launch",
+				name = "RunOrTest",
+				metals = {
+					runType = "runOrTestFile",
+					--args = { "firstArg", "secondArg", "thirdArg" }, -- here just as an example
+				},
+			},
+			{
+				type = "scala",
+				request = "launch",
+				name = "Test Target",
+				metals = {
+					runType = "testTarget",
+				},
+			},
+		}
+
+		metals_config.on_attach = function(client, bufnr)
+			-- Call setup_dap without arguments, or explicitly pass bufnr if it's a number
+			if type(bufnr) == "number" then
+				require("metals").setup_dap()
+			else
+				-- In some versions, bufnr might be a function to get the buffer
+				require("metals").setup_dap()
+			end
+		end
+		-- metals_config.on_attach = function(client, bufnr)
+		-- require("metals").setup_dap()
+		-- end
 		return metals_config
 	end,
 	config = function(self, metals_config)
